cbb7bd30195bcdc3bb1d642a32d1b55af0401d64
Старунов Андрей Сергеевич <astarunov@ozon.ru>
2025-06-18 23:45:33 +0300

    add tables module

--- src/database.py
+++ src/database.py
@@ -1,53 +1,23 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
-from sqlalchemy import (
-    create_engine,
-    Column,
-    Integer,
-    String,
-    BigInteger,
-    TIMESTAMP,
-    Boolean
-)
-
-from sqlalchemy.orm import (
-    declarative_base,
-    Session
-)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from config import (
     DB_USER,
     DB_PASSWORD,
     DB_NAME,
 )
+from src.tables import (
+    Base,
+    Customers,
+    Deals
+)
 
 engine = create_engine(
     url=f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}"
 )
-
-Base = declarative_base()
-
-
-class Customers(Base):
-    __tablename__ = "customers"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    telegram_id = Column(BigInteger)
-    chat_id = Column(BigInteger, default=0)
-    expired_at = Column(TIMESTAMP)
-    created_at = Column(TIMESTAMP, default=datetime.now())
-
-
-class Deals(Base):
-    __tablename__ = "deals"
-
-    id = Column(Integer, primary_key=True)
-    customer_telegram_id = Column(BigInteger)
-    service_months = Column(Integer)
-    is_success = Column(Boolean, default=False)
-    created_at = Column(TIMESTAMP, default=datetime.now())
 
 
 def add_row(table: Base, **kwargs):
---
+++ src/tables.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    BigInteger,
+    TIMESTAMP,
+    Boolean
+)
+
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Customers(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    telegram_id = Column(BigInteger)
+    chat_id = Column(BigInteger, default=0)
+    expired_at = Column(TIMESTAMP)
+    created_at = Column(TIMESTAMP, default=datetime.now())
+
+
+class Deals(Base):
+    __tablename__ = "deals"
+
+    id = Column(Integer, primary_key=True)
+    customer_telegram_id = Column(BigInteger)
+    service_months = Column(Integer)
+    is_success = Column(Boolean, default=False)
+    created_at = Column(TIMESTAMP, default=datetime.now())
